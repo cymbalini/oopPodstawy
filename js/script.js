d6ff4ef282a878b451f3c69af5e4c31347599cc1
Cymbal <112689825+cymbalini@users.noreply.github.com>
2023-05-10 16:23:12 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//starszy nie stosujemy
+
 // const login ='pkania'
 // const name ='Kania'
 // const firstname ='Piotr'
@@ -20,6 +22,12 @@
 // console.log(persone1.age)
 // persone1.sayHallo()
 
+
+
+
+
+//nowszy (stosujemy)
+
 function User(login,name,age,boss){
     this.login=login
     this.name=name
@@ -33,3 +41,23 @@ const user1=new User('akowalska','anna',30,false)
 user1.sayHallo()
 const user2=new User('jnowak','jan',40,true)
 user2.sayHallo()
+
+
+
+// klasa to to samo co konstruktor
+// (stosujemy)
+
+class User2{
+    constructor(login,name,age,boss){
+        this.login=login
+        this.name=name
+        this.age=age
+        this.boss=boss
+        this.sayHallo
+    }
+    sayHallo(){
+        console.log(`Czesc tu ${this.name}`)
+    }
+}
+const user3=new User2('enowak','nowak',20,false)
+user3.sayHallo()
